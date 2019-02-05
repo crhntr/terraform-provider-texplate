2cfa94a28305516a83cfa6dd65fc02dd93d3b00f
Christopher Hunter <hunter.christopher@icloud.com>
2019-02-04 23:04:03 -0800

    Create More Complicated Example
    - does not work yet
    - currently it is failing gloriously
    - next steps include
      - adding err test after parse
      - adding vars to schema
      - ensuring unique ids
      - consider nameing templates
      - write CI
      - investigate how to package and release providers

--- example.tf
+++ example.tf
@@ -1,9 +1,39 @@
 provider "texplate" {}
 
-data "texplate_execute" "test" {
+data "texplate_execute" "greeting" {
   template = "Hello, world!"
 }
 
-output "test" {
-  value = "${data.texplate_execute.test.output}"
+output "greeting" {
+  value = "${data.texplate_execute.greeting.output}"
+}
+
+data "local_file" "director_config_template" {
+  filename = "${path.module}/testdata/configure-director.yml"
+}
+
+data "texplate_execute" "director_config" {
+  template = "${data.local_file.director_config_template.content}"
+
+  vars {
+    "subscription_id"               = "some-sub-id"
+    "tenant_id"                     = "some-tenant-id"
+    "client_id"                     = "some-client-id"
+    "client_secret"                 = "some-client-secret"
+    "pcf_resource_group_name"       = "floating-pods"
+    "bosh_root_storage_account"     = "bosh-root-storage-account"
+    "ops_manager_ssh_public_key"    = "some-ssh-public-key"
+    "ops_manager_ssh_private_key"   = "-----BEGIN RSA PRIVATE KEY-----\n701\n-----END RSA PRIVATE KEY-----\n"
+    "infrastructure_subnet_name"    = "floating-pods-infra-subnet"
+    "infrastructure_subnet_cidr"    = "10.0.8.0/26"
+    "infrastructure_subnet_gateway" = "10.0.8.1"
+    "network_name"                  = "floating-pods-virtual-network"
+    "control_plane_subnet_cidr"     = "10.0.10.0/28"
+    "control_plane_subnet_name"     = "floating-pods-plane-subnet"
+    "control_plane_subnet_gateway"  = "10.0.10.0"
+  }
+}
+
+output "director_config" {
+  value = "${data.texplate_execute.director_config.output}"
 }
